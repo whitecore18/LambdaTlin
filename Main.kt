6955b8d8fd7550a582c7aba144ba678eb975462a
whitecore18 <whitecore18@gmail.com>
2024-12-19 17:48:58 +0400

    Update Main.kt

--- Main.kt
+++ Main.kt
@@ -1,43 +1,39 @@
 fun main() {
-    // 1. Запускает лямбду print() заданное количество раз
+    // 1. Повторное выполнение действия
     fun repeatPrint(count: Int, print: () -> Unit) {
         for (i in 1..count) {
             print()
         }
     }
 
-    // Пример использования repeatPrint
     repeatPrint(3) { println("Hello, World!") }
 
-    // 2. Функция для вычисления среднего арифметического списка чисел
-    fun average(numbers: List<Double>): Double {
-        return if (numbers.isNotEmpty()) {
+    // 2. Среднее арифметическое
+    val average: (List<Double>) -> Double = { numbers ->
+        if (numbers.isNotEmpty()) {
             numbers.sum() / numbers.size
         } else {
             0.0
         }
     }
 
-    // Пример использования average
     val numbers = listOf(1.0, 2.0, 3.0, 4.0, 5.0)
     println("Среднее арифметическое: ${average(numbers)}")
 
-    // 3. Функция для фильтрации положительных чисел
-    fun filterPositive(numbers: IntArray): IntArray {
-        return numbers.filter { it > 0 }.toIntArray()
+    // 3. Фильтрации положительных чисел
+    val filterPositive: (IntArray) -> IntArray = { numbers ->
+        numbers.filter { it > 0 }.toIntArray()
     }
 
-    // Пример использования filterPositive
     val array = intArrayOf(-1, 2, -3, 4, 5)
     val positiveArray = filterPositive(array)
     println("Положительные числа: ${positiveArray.joinToString()}")
 
-    // 4. Функция для обработки массива целых чисел
-    fun processArray(numbers: IntArray): IntArray {
-        return numbers.map { if (it % 2 == 0) it / 2 else it * 3 }.toIntArray()
+    // 4. Обработка массива целых чисел
+    val processArray: (IntArray) -> IntArray = { numbers ->
+        numbers.map { if (it % 2 == 0) it / 2 else it * 3 }.toIntArray()
     }
 
-    // Пример использования processArray
     val intArray = intArrayOf(1, 2, 3, 4, 5)
     val processedArray = processArray(intArray)
     println("Обработанный массив: ${processedArray.joinToString()}")
